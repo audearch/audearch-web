6463ec7ee80cb51e84799a8ca528505c5ded6323
peijun <contact.zouri.music@gmail.com>
2020-08-24 23:02:54 +0900

    fix: add database system

--- audearch-web/cruds.py
+++ audearch-web/cruds.py
@@ -4,9 +4,9 @@ from schemas import MusicData, MusicMetadata
 def music_register(imongo, music_data: MusicData) -> None:
 
     for landmark in music_data.music_landmark:
-        imongo.insert_music(music_data.music_id, landmark[0], landmark[1])
+        imongo.insert_music(music_data.music_id, str(landmark[0]), int(landmark[1]))
 
 
 def music_metadata_register(imongo, music_metadata: MusicMetadata) -> None:
 
-    imongo.insert_music_metadata(music_metadata)
+    imongo.insert_music_metadata(music_metadata.music_id, music_metadata.title, music_metadata.duration)
