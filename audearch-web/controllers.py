ae8f0a892c465bc2fbbbf4423a7ed3d72f6a33f3
peijun <contact.zouri.music@gmail.com>
2020-08-24 23:02:31 +0900

    perf: background化

--- audearch-web/controllers.py
+++ audearch-web/controllers.py
@@ -2,7 +2,7 @@ import time
 
 from audearch.analyzer import analyzer
 from audearch.database import MongodbFactory
-from fastapi import FastAPI, File, UploadFile
+from fastapi import BackgroundTasks, FastAPI, File, UploadFile
 from starlette.requests import Request
 from starlette.templating import Jinja2Templates
 
@@ -19,28 +19,32 @@ templates = Jinja2Templates(directory="templates")
 jinja_env = templates.env
 
 
-def index(request: Request):
-    return templates.TemplateResponse('index.html', {'request': request})
-
-
-@app.post("/upload_file/", status_code=201)
-async def upload_file(files: UploadFile = File(...)):
+async def write_hash(files: UploadFile, title: str, music_id: int, duration: int) -> None:
     mongodb = MongodbFactory()
     imongo = mongodb.create()
 
-    music_id = int(str(time.time())[-6:])
-
     file_object = files.file
     list_landmark = analyzer(file_object)
-
-    title = "test"
-    duration = 0
 
     music = MusicData(music_id, list_landmark)
     music_meta = MusicMetadata(music_id, title, duration)
 
     music_register(imongo, music)
     music_metadata_register(imongo, music_meta)
+
+
+def index(request: Request):
+    return templates.TemplateResponse('index.html', {'request': request})
+
+
+@app.post("/upload_file/", status_code=201)
+async def upload_file(background_tasks: BackgroundTasks, files: UploadFile = File(...)):
+    music_id = int(str(time.time())[-6:])
+
+    title = "test"
+    duration = 0
+
+    background_tasks.add_task(write_hash, files, title, music_id, duration)
 
     return {"music_id": music_id}
 
